8dae2df65573c29a3f74babc44521fe3f9127a46
Mike Miller <millms302@gmail.com>
2026-03-05 21:56:56 -0600

    end of class 5

--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,19 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+import certifi
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Create a secure connection to the email server.
+os.environ['SSL_CERT_FILE'] = certifi.where()
+
+# Load environment variables to the env file
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -121,3 +130,12 @@ STATICFILES_DIRS=[BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import ContactForm
+from django.core.mail import send_mail
 
 # Create your views here.
 
@@ -10,3 +12,16 @@ def experience_view(request):
 
 def project_view(request):
     return render(request, 'projects/projects.html')
+
+def contact_view(request):
+    if request.method == 'POST':
+        # means the form has been submitted
+        form = ContactForm(request.POST)
+        # Collect the data from the form and validate it.
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+        
+    form = ContactForm()
+    return render(request, 'pages/contact.html', {'form': form})
